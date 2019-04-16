d99887471eb7f605945d9226cc88c6cf48dc95e0
Tyler Harriott <tharriott02@gmail.com>
2019-04-16 10:11:09 -0400

    Create if_else.js

---
+++ Week-1/Day-1/if_else.js
@@ -0,0 +1,30 @@
+/**********************************
+ If / Else Statement
+ **********************************/
+
+
+
+var firstName = "John";
+var civilStatus = "single";
+
+if(civilStatus === "married"){
+
+    console.log(firstName + "Is married!");
+}
+else{
+    console.log(firstName + "No, but hopefully marry soon!");
+}
+
+
+////////////////////////////////////////////////////////////////
+
+
+var isMarried = false;
+
+if(isMarried){
+
+    console.log(firstName + "Is married!");
+}
+else{
+    console.log(firstName + "No, but hopefully marry soon!");
+}
