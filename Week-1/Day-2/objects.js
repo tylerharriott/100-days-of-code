4b2e3a4be9a7b99a08b55d7947cdabb6cd6afc06
Tyler Harriott <tharriott02@gmail.com>
2019-04-16 20:04:13 -0400

    Create objects.js

---
+++ Week-1/Day-2/objects.js
@@ -0,0 +1,36 @@
+/****************************
+            Objects
+ ****************************/
+
+
+//Objects -> Key and Value (Object Literal)
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',                      //An object can contain any type of data
+    birthYear: 1990,
+    family: ['Jane','Mark','Bob','Emily'],
+    job: 'teacher',
+    isMarried: false
+
+
+};
+
+console.log(john.firstName);
+
+//To read the value of the keys, just have to use "." and then the key name.
+
+console.log(john['lastName']);
+
+
+
+//Mutate
+john.job = 'designer';
+john['isMarried'] = true;       //The
+
+
+
+//new Object Syntax
+var jane = new Object();
+jane.firstName = 'Jane';
+jane.birthYear = '1969';
+jane['lastName'] = 'Smith'
