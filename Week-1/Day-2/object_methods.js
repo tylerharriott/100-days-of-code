60a210c98575ed50687e241a9e67f9f620723f63
Tyler Harriott <tharriott02@gmail.com>
2019-04-16 20:06:46 -0400

    Create object_methods.js

---
+++ Week-1/Day-2/object_methods.js
@@ -0,0 +1,29 @@
+/**************************************
+        Object and Methods
+ **************************************/
+
+
+//Only objects have methods
+
+    
+//Object   
+var john = {
+    firstName: 'John',
+    lastName: 'Smith',                      //An object can contain any type of data
+    birthYear: 1990,
+    family: ['Jane','Mark','Bob','Emily'],
+    job: 'teacher',
+    isMarried: false,
+    calcAge: function(){                    //An object can contain functions
+        return 2019 - this.birthYear;
+    }
+
+};
+
+
+//Methods
+var age = john.calcAge(1960);
+john.age = age;
+
+
+
